50da6633a7b1ace30d9f06ba2aa947c4d1e32dc0
Luke Davids <lukedavids@gmail.com>
2025-03-17 10:33:19 -0500

    Created Pottery

---
+++ src/scripts/PotteryWheel.js
@@ -0,0 +1,12 @@
+
+let primaryKey = 1
+
+export const makePottery = (shape, weight, height) => {
+    return {
+        shape: shape,
+        weight: weight,
+        height: height,
+        id: primaryKey++
+    }
+
+}
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,15 @@
-// Imports go first
+import { makePottery } from "./PotteryWheel.js";
 
 
 
 // Make 5 pieces of pottery at the wheel
-let mug = makePottery()
+let pottery1 = makePottery("mug", 3, 25)
+let pottery2 = makePottery ("plate", 1, 35)
+let pottery3 = makePottery("platter", 2, 60)
+let pottery4 = makePottery("vase", 7, 55)
+let pottery5 = makePottery("bowl", 4, 40)
 
-
+console.log(pottery1, pottery2, pottery3, pottery4, pottery5)
 // Fire each piece of pottery in the kiln
 
 
